cc9d1aaad787cfb79a5bd72c19b0c998c2259007
oinsup <oinsup@gmail.com>
2022-01-11 22:46:06 +0900

    이벤트 추가

--- src/ImgBox.js
+++ src/ImgBox.js
@@ -1,9 +1,20 @@
 import React,{useState} from 'react';
 
-
 const ImgBox = (props) => {
     const [imgSrc,setImgSrc] = useState({name:null,src:null});
+    const [winSize,setWinSize] = useState();
     
+    window.addEventListener("resize", function(){
+        setWinSize(window.outerWidth);
+    });
+    document.addEventListener("DOMContentLoaded", function(){
+        setWinSize(window.outerWidth);
+
+    });
+        
+        
+
+
     const onChange = (e) => {
         let reader = new FileReader();
         let file = e.target.files[0];
@@ -18,7 +29,7 @@ const ImgBox = (props) => {
                 {
                     imgSrc.name === null ? 
                     <input type="file" onChange={onChange} accept='image/*'/> : 
-                    <img src={imgSrc.src} alt={imgSrc.name}/>
+                    <img src={imgSrc.src} alt={imgSrc.name} style={{width:winSize}}/>
                 }
             </div>
         </>
