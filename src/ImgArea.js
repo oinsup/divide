475e5a19b7b2946e88faa59e9657e658ce37328f
oinsup <oinsup@gmail.com>
2022-01-13 01:22:28 +0900

    디자인 변경

--- src/ImgArea.js
+++ src/ImgArea.js
@@ -9,6 +9,40 @@ const ImgGroup = styled.div`
     display:flex;
 `
 
+const Handler = styled.div`
+   position:absolute;
+   left:50%;
+   top:0;
+   z-index:10;
+   width:4px;
+   height:100%;
+   background:#ccc;
+   transform:translateX(-50%);
+   span{
+    position:absolute;
+    left:50%;top:50%;
+    transform:translate(-50%,-50%);
+    width:40px;
+    height:40px;
+    background:#fff;
+    border:2px solid #ccc;
+    border-radius:10px;
+    font-size:0;
+    &:before,&:after{
+        position:absolute;
+        top:50%;
+        transform:translateY(-50%);
+        width:3px;
+        height:30px;
+        background:#aaa;
+        display:block;
+        content:'';
+    }
+    &:before{left:10px;}
+    &:after{right:10px;}
+   }
+`
+
 
 const ImgArea = () => {
     const [pos,setPos] = useState()
@@ -21,9 +55,9 @@ const ImgArea = () => {
     return (
         <ImgGroup>
             <ImgBox width={(pos / window.innerWidth) * 100 + "%"}/>
-            <div className='handler' style={{left: (pos / window.innerWidth) * 100 + "%"}}>
+            <Handler className='handler' style={{left: (pos / window.innerWidth) * 100 + "%"}}>
                 <span onDragEnd={onDragEnd} onTouchEnd={onTouchEnd} onDrag={onDragEnd} onTouchMove={onTouchEnd} draggable>handle</span>
-            </div>
+            </Handler>
             <ImgBox/>
         </ImgGroup>
     );
--- src/ImgBox.js
+++ src/ImgBox.js
@@ -1,4 +1,12 @@
 import React,{useState} from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+    position:absolute;
+    left:50%;
+    top:50%;    
+    transform:translate(-50%,-50%);
+`
 
 const ImgBox = (props) => {
     const [imgSrc,setImgSrc] = useState({name:null,src:null});
@@ -11,9 +19,6 @@ const ImgBox = (props) => {
         setWinSize(window.outerWidth);
 
     });
-        
-        
-
 
     const onChange = (e) => {
         let reader = new FileReader();
@@ -27,8 +32,8 @@ const ImgBox = (props) => {
         <>
             <div className='divBox' style={{width:props.width}}>
                 {
-                    imgSrc.name === null ? 
-                    <input type="file" onChange={onChange} accept='image/*'/> : 
+                    imgSrc.name === null ?
+                    <Input type="file" onChange={onChange} accept='image/*'/> :
                     <img src={imgSrc.src} alt={imgSrc.name} style={{width:winSize}}/>
                 }
             </div>
